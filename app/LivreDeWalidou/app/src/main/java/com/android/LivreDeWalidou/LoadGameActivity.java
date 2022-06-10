a04045136c0dfb49482362ce1d67344b2c06f846
Walid-Ouldali <ouldali02@hotmail.fr>
2022-06-10 20:05:56 +0200

    Adding main layout and LoadGame layout

---
+++ app/LivreDeWalidou/app/src/main/java/com/android/LivreDeWalidou/LoadGameActivity.java
@@ -0,0 +1,14 @@
+package com.android.LivreDeWalidou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_game);
+    }
+}
--- app/LivreDeWalidou/app/src/main/java/com/android/LivreDeWalidou/view/LoadGameActivity.java
+++
@@ -1,6 +0,0 @@
-package com.android.LivreDeWalidou.view;
-
-import com.android.LivreDeWalidou.model.repository.GameRepository;
-
-public class LoadGameActivity {
-}
