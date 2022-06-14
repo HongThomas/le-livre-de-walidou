f570d6d99d46f4e51e64e0eda8059619eaa68f81
Walid-Ouldali <ouldali02@hotmail.fr>
2022-06-14 20:39:48 +0200

    Adding navbar et button vers second activity

--- app/LivreDeWalidou/app/src/main/java/com/android/LivreDeWalidou/MainActivity.java
+++ app/LivreDeWalidou/app/src/main/java/com/android/LivreDeWalidou/MainActivity.java
@@ -2,15 +2,26 @@ package com.android.LivreDeWalidou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.LivreDeWalidou.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button load;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        load = findViewById(R.id.button2);
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoadGameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
